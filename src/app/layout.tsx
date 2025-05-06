c68dfcf1ca9904c19c89ce6ba1aaf5222ed4dabd
o-ga09 <taitiabe1997@gmail.com>
2025-05-06 11:03:25 +0900

    ✨ feat: Google Adsenseのインポート方法を修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Footer } from "@/components/layout/footer";
 import { APP_URL } from "@/lib/constant";
 import type { Metadata } from "next";
 import GoogleAnalytics from "@/components/googleanalytics";
-import { GoogleAdsense } from "@/components/googleAdsense";
+import GoogleAdsense from "@/components/googleAdsense";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
